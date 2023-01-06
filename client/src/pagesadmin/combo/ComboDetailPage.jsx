0fc9efce5454de1e16c43d23ff4358987f5793f2
LanChuHoang <hoanglan.ict@gmail.com>
2023-01-06 10:01:45 +0700

    update combo detail page, combo list page

--- client/src/pagesadmin/combo/ComboDetailPage.jsx
+++ client/src/pagesadmin/combo/ComboDetailPage.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import AdminLayout from "src/layout/AdminLayout";
+import styled from "styled-components";
+
+const ContentWrapper = styled.div`
+  & .content-top {
+    margin-bottom: 24px;
+  }
+  & .content-middle {
+    background: #fff;
+    padding: 24px;
+  }
+`;
 
 const ComboDetailPage = () => {
   return <AdminLayout>ComboDetailPage</AdminLayout>;
