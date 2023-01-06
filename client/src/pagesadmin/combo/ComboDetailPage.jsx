512e406f64d95c693f8a17cd04f002f28d35d00e
LanChuHoang <hoanglan.ict@gmail.com>
2023-01-06 09:34:34 +0700

    add combo detail page base

---
+++ client/src/pagesadmin/combo/ComboDetailPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AdminLayout from "src/layout/AdminLayout";
+
+const ComboDetailPage = () => {
+  return <AdminLayout>ComboDetailPage</AdminLayout>;
+};
+
+export default ComboDetailPage;
