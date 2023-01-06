93fa709769ac890ae4af7b4c787c3e179bd46015
ledaiviet <ledaiviet.1610@gmail.com>
2023-01-06 09:20:03 +0700

    add interface

--- client/src/pagesadmin/user/UserListPage.jsx
+++ client/src/pagesadmin/user/UserListPage.jsx
@@ -48,4 +48,136 @@ const UserListPage = () => {
   const handleLocalSearch = (values) => {
     setUsersFilterValue({ ...usersFilterValue, keyword: values.keySearch });
   };
-}
+  const columns = [
+    {
+      title: "Người dùng",
+      dataIndex: "name",
+      key: "name",
+      ellipsis: true,
+      render: (text, record) => (
+        <Space>
+          <Avatar
+            size={48}
+            src={record.picture}
+            fallback={NOT_FOUND_IMG}
+            key={`table_${record._id}`}
+          />
+          <Space size={2} direction="vertical">
+            <Space size={4} wrap={false}>
+              <Typography.Text ellipsis>{text}</Typography.Text>
+              <Tag>{record.role}</Tag>
+            </Space>
+            <Typography.Text ellipsis>{record.email}</Typography.Text>
+          </Space>
+        </Space>
+      ),
+      sorter: (a, b) => sorterByWords("name")(a, b),
+    },
+    {
+      title: "Ngày tham gia",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      width: 240,
+      render: (text, record) => (
+        <Typography.Text ellipsis>{formatDate(text, "DD-MM-YYYY HH:mm:ss")}</Typography.Text>
+      ),
+    },
+    {
+      title: "Ngày cập nhật",
+      dataIndex: "updatedAt",
+      key: "updatedAt",
+      width: 240,
+      render: (text, record) => (
+        <Typography.Text ellipsis>{formatDate(text, "DD-MM-YYYY HH:mm:ss")}</Typography.Text>
+      ),
+    },
+    {
+      title: <BsThreeDots size={24} />,
+      dataIndex: "",
+      key: "action",
+      width: 180,
+      render: (text, record) => (
+        <Space size="middle">
+          {isActive(record.status) ? (
+            <Popconfirm
+              title={
+                <p style={{ margin: 0 }}>
+                  Bạn chắc chắn muốn khoá tài khoản <b>{record._id}</b> ?
+                </p>
+              }
+              placement="topRight"
+              okText={<BsCheckLg />}
+              cancelText={<BsXLg />}
+              onConfirm={() => handleLock(record._id)}
+            >
+              <Button size="large" type="text" danger icon={<FaLock />}></Button>
+            </Popconfirm>
+          ) : (
+            <Button
+              size="large"
+              type="link"
+              onClick={() => handleUnlock(record._id)}
+              icon={<FaUnlockAlt />}
+            ></Button>
+          )}
+        </Space>
+      ),
+    },
+  ];
+  return (
+    <AdminLayout>
+      <ContentWrapper>
+        <LocalSearch
+          placeholder="Tìm kiếm người dùng theo Tên, email..."
+          onFinish={handleLocalSearch}
+          onValuesChange={handleLocalSearch}
+        />
+        <Row gutter={24} wrap={false}>
+          <Col flex="auto">
+            <Card title="Danh sách người dùng" loading={!getUsersSuccess} bodyStyle={{ padding: 0 }}>
+              {getUsersSuccess && (
+                <Table
+                  className="table-fixed-pagination"
+                  rowKey={(record) => record._id}
+                  columns={columns}
+                  footer={() => ""}
+                  dataSource={usersFilteredData}
+                  size="default"
+                  rowClassName={(record) => record._id === user._id && "disabled-row"}
+                  pagination={{
+                    total: usersFilteredData.length,
+                    showTotal: (total) => (
+                      <p style={{ marginRight: 16 }}>
+                        Tổng <b>{total}</b>
+                      </p>
+                    ),
+                    defaultPageSize: 10,
+                    showSizeChanger: true,
+                    pageSizeOptions: ["10", "20", "30", "50"],
+                  }}
+                />
+              )}
+            </Card>
+          </Col>
+        </Row>
+      </ContentWrapper>
+    </AdminLayout>
+  );
+};
+
+const ContentWrapper = styled.div`
+  margin-bottom: 68px;
+  & .content-top {
+    margin-bottom: 24px;
+  }
+  & .content-middle {
+    background: #fff;
+    padding: 24px;
+  }
+  & .disabled-row {
+    background-color: #f5f5f5;
+    pointer-events: none;
+  }
+`;
+
+export default UserListPage;
