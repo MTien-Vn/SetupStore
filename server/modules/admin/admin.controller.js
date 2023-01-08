2815bc82ce94db2b4bb24716192d7a6a158650e2
Manh Tien <57245180+MTien-Vn@users.noreply.github.com>
2023-01-08 13:17:24 +0000

    hotfix bug

--- server/modules/admin/admin.controller.js
+++ server/modules/admin/admin.controller.js
@@ -5,18 +5,12 @@ const User = require("../user/user");
 
 exports.getAllOrders = async (req, res) => {
   let orders = await Order.find({})
-    .populate("products.product")
-    .populate("orderedBy", "_id name picture area address")
-    .sort([["createdAt", "desc"]])
-    .exec();
+  .populate("products.product")
+  .populate("orderedBy", "_id name picture area address")
+  .sort([["createdAt", "desc"]])
+  .exec();
 
   res.json(orders);
 };
 
-exports.updateOrderStatus = async (req, res) => {
-  const { orderId, orderStatus } = req.body;
 
-  let updated = await Order.findByIdAndUpdate(orderId, { orderStatus }, { new: true }).exec();
-
-  res.json(updated);
-};
